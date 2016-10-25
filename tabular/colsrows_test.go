4285a3d36936b21b1a1a10a2e14c98f61b7c7efd
JP Fielding <jp.fielding@gmail.com>
2016-10-25 16:01:51 +0000

    making use of go1.7 tests

--- tabular/colsrows_test.go
+++ tabular/colsrows_test.go
@@ -15,18 +15,19 @@ func TestColumns(t *testing.T) {
 	}
 
 	cols := Columns([]string{"id", "status", "price"})
-
 	index := cols.Index()
-	map1 := rows[0].Map(cols, index)
-	testutils.Equals(t, "one", map1["id"])
-	testutils.Equals(t, "active", map1["status"])
-	testutils.Equals(t, "234234", map1["price"])
 
-	map3 := rows[2].Map(cols, index)
-	testutils.Equals(t, "three", map3["id"])
-	testutils.Equals(t, "pending", map3["status"])
-	testutils.Equals(t, "9834543", map3["price"])
-
+	test := func(row int, id, status, price string) func(*testing.T) {
+		return func(tt *testing.T) {
+			m := rows[row].Map(cols, index)
+			testutils.Equals(tt, id, m["id"])
+			testutils.Equals(tt, status, m["status"])
+			testutils.Equals(tt, price, m["price"])
+		}
+	}
+	t.Run("cols", test(0, "one", "active", "234234"))
+	t.Run("cols", test(1, "two", "sold", "543234"))
+	t.Run("cols", test(2, "three", "pending", "9834543"))
 }
 
 func TestRowSort(t *testing.T) {
@@ -39,21 +40,35 @@ func TestRowSort(t *testing.T) {
 	cols := Columns([]string{"id", "status", "price"})
 	index := cols.Index()
 
-	sortBy := SortRows{
-		Compare: CompareAsInts,
-		Row:     rows,
-		SortBy:  "price",
-		Indexer: index,
+	test := func(row int, id, status, price string) func(*testing.T) {
+		return func(tt *testing.T) {
+			m := rows[row].Map(cols, index)
+			testutils.Equals(tt, id, m["id"])
+			testutils.Equals(tt, status, m["status"])
+			testutils.Equals(tt, price, m["price"])
+		}
 	}
+
+	sortBy := SortRows{
+		Row:     rows,
+		Indexer: index,
+		Compare: CompareAsInts,
+		SortBy:  "price",
+	}
+
+	sortBy.Compare = CompareAsInts
+	sortBy.SortBy = "price"
 	sort.Sort(sortBy)
-	testutils.Equals(t, "three", rows[0].Map(cols, index)["id"])
-	testutils.Equals(t, "two", rows[1].Map(cols, index)["id"])
-	testutils.Equals(t, "one", rows[2].Map(cols, index)["id"])
+
+	t.Run("by price", test(0, "three", "pending", "334543"))
+	t.Run("by price", test(1, "two", "sold", "7543234"))
+	t.Run("by price", test(2, "one", "active", "9234234"))
 
 	sortBy.Compare = nil
 	sortBy.SortBy = "id"
 	sort.Sort(sortBy)
-	testutils.Equals(t, "one", rows[0].Map(cols, index)["id"])
-	testutils.Equals(t, "three", rows[1].Map(cols, index)["id"])
-	testutils.Equals(t, "two", rows[2].Map(cols, index)["id"])
+
+	t.Run("by id", test(1, "three", "pending", "334543"))
+	t.Run("by id", test(2, "two", "sold", "7543234"))
+	t.Run("by id", test(0, "one", "active", "9234234"))
 }
